4b204cdba4e57ce7e9eec8557767a5d614a2a8ca
siddheshghare <siddheshghare6112@gmail.com>
2025-08-04 22:32:46 +0530

    footer fixed

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -3,21 +3,27 @@ import { assets } from '../../assets/assets'
 
 function Footer() {
   return (
-    <footer className='flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t'>
+    <footer className='flex md:flex-row flex-col-reverse items-center justify-between w-full px-8 border-t py-4'>
+      {/* Left Section (Logo + Divider + Copyright) */}
       <div className='flex items-center gap-4'>
-        <img className='hidden md:block w-20 ' src={assets.logo} alt="" />
-        <div className='hidden md:block h-7 w-px bg-gray-500/60'>
-          <p className='py-4 text-center text-xs md:text-sm text-gray-600'>
-            Copyright 2025 @ SidGhare,All Right Reserved
-          </p>
-        </div>
-        <div  className='flex items-center gap-3 max-md:mt-4'>
-          <a href="#"><img src={assets.facebook_icon} alt="" /></a>
-          <a href="#"><img src={assets.instagram_icon} alt="" /></a>
-          <a href="#"><img src={assets.twitter_icon} alt="" /></a>
-        </div>
+        {/* Logo */}
+        <img className='hidden md:block w-20' src={assets.logo} alt="Logo" />
+
+        {/* Divider */}
+        <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
+
+        {/* Copyright */}
+        <p className='text-xs md:text-sm text-gray-600'>
+          Copyright 2025 @ SidGhare, All Rights Reserved
+        </p>
       </div>
 
+      {/* Right Section (Social Icons) */}
+      <div className='flex items-center gap-3 mt-4 md:mt-0'>
+        <a href="#"><img src={assets.facebook_icon} alt="Facebook" /></a>
+        <a href="#"><img src={assets.instagram_icon} alt="Instagram" /></a>
+        <a href="#"><img src={assets.twitter_icon} alt="Twitter" /></a>
+      </div>
     </footer>
   )
 }
