2d6bc847463fea4851beec1feb62a828ae870c51
siddheshghare <siddheshghare6112@gmail.com>
2025-08-22 20:09:27 +0530

    changes in webHooks

--- server/controllers/webHooks.js
+++ server/controllers/webHooks.js
@@ -5,12 +5,12 @@ import User from "../models/user.model.js";
 export const clerkWebhooks = async (req,res)=>{
     try {
         const wHook = new Webhook(process.env.CLERK_SECRET)
-        await wHook.verify(JSON.stringify(req.body,{
+        await wHook.verify(JSON.stringify(req.body),{
             "svix-id":req.headers["svix-id"],
             "svix-timestamp":req.headers["svix-timestamp"],
             "svix-signature":req.headers["svix-signature"]
 
-        }))
+        })
 
         const {data,type } = req.body
 
@@ -18,7 +18,7 @@ export const clerkWebhooks = async (req,res)=>{
             case "user.created":{
                 const userData ={
                     _id:data.id,
-                    email:data.email_address[0].email_address,
+                    email:data.email_addresses[0].email_address,
                     name:data.first_name +" "+data.last_name,
                     imageUrl:data.image_url
 
@@ -30,13 +30,13 @@ export const clerkWebhooks = async (req,res)=>{
 
             case "user.updated":{
                  const userData ={
-                    email:data.email_address[0].email_address,
+                    email:data.email_addresses[0].email_address,
                     name:data.first_name +" "+data.last_name,
                     imageUrl:data.image_url
 
                 }
                 await User.findByIdAndUpdate(data.id,userData)
-                res.json("updated")
+               res.json({ message: "User updated" });
                 break;
 
             }
